378e25708af366922b30d7b438078ee6d8b86b64
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-12-14 04:48:11 +0000

    move to ovos

--- setup.py
+++ setup.py
@@ -1,21 +1,47 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import os
+from os import walk, path
 
+
+URL = "https://github.com/OpenVoiceOS/skill-ovos-youtube-music"
+SKILL_CLAZZ = "YoutubeMusicSkill"  # needs to match __init__.py class name
+PYPI_NAME = "skill-youtube-music"  # pip install PYPI_NAME
+
+
+# below derived from github url to ensure standard skill_id
+SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
+SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
+PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'skill-youtube-music.jarbasai=skill_youtube_music:YoutubeMusicSkill'
+
+def find_resource_files():
+    # add any folder with files your skill uses here! 
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex", "skill")
+    base_dir = path.dirname(__file__)
+    package_data = ["*.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(base_dir, res)):
+            for (directory, _, files) in walk(path.join(base_dir, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(base_dir, "").lstrip('/'),
+                                  '*'))
+    return package_data
+
 
 setup(
-    # this is the package name that goes on pip
-    name='skill-youtube-music',
-    version='0.0.1',
+    name=PYPI_NAME,
+    version="0.0.1",
+    url=URL,
+    license='Apache-2.0',
+    package_dir={SKILL_PKG: ""},
+    package_data={SKILL_PKG: find_resource_files()},
+    packages=[SKILL_PKG],
     description='ovos common play youtube music skill plugin',
-    url='https://github.com/JarbasSkills/skill-youtube-music',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
-    package_dir={"skill_youtube_music": ""},
-    package_data={'skill_youtube_music': ['locale/*', 'ui/*']},
-    packages=['skill_youtube_music'],
     include_package_data=True,
     install_requires=["ovos-plugin-manager>=0.0.1a3",
                       "tutubo",
